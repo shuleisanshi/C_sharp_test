57f9e5e0f677e2fba89827aa08541a1dd3344b1a
shulei <shuleisanshi@163.com>
2022-12-11 18:17:25 +0900

    024 类的声明控制与访问级别

---
+++ WindowsFormsApp1/MyLib/MyNamespace2/Student.cs
@@ -0,0 +1,15 @@
+﻿using MyLib.MyNamespace;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib.MyNamespace2
+{
+    // 默认的class是internal, 这是项目级别内的可引用
+    // 项目级别,即 Assembly, 即 .dll
+    // Assembly, 一般有两种, .exe 和 .dll
+    class Student
+    {
+        public Calculator Calculator { get; set; }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyLib.MyNamespace;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            Calculator cal = new Calculator();
+            double res = cal.Add(1.1, 2.2);
+            Console.WriteLine(res);
+
+        }
+    }
+}
